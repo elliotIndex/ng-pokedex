6e6028e6ae4f29ea29912dc81d9d5dc925327ff2
Elliot Plant <elliot@index.com>
2017-05-22 17:47:43 -0700

    Get bstack working with chrome i guess

---
+++ bsCredentials.template.js
@@ -0,0 +1,7 @@
+// Copy this file and fill it out with the "BrowserStack Credentials" found in 1password
+// DO NOT put the info in this file. Put it in the copied file.
+// This prevents our (expensive) browserstack keys from possibly being released into the wild
+module.exports = {
+  user: 'put user here',
+  key: 'put key here'
+};
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,30 +1,73 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var browserstack = require('browserstack-local');
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
+  seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
+  'commonCapabilities': {
+    'browserstack.user': 'browserstackusername',
+    'browserstack.key': 'browserstackkey',
+    'build': 'protractor-browserstack',
+    'name': 'parallel_local_test',
+    'browserstack.local': true,
+    'browserstack.debug': 'true'
   },
-  directConnect: true,
+  'multiCapabilities': [{
+    'browserName': 'Chrome'
+  }, {
+    'browserName': 'Firefox'
+  }, {
+    'browserName': 'Safari'
+  }],
+  // directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 20000,
     print: function() {}
   },
   beforeLaunch: function() {
+    // Start browserstack local before start of test
+    console.log("Connecting local");
+    return new Promise(function(resolve, reject){
+      exports.bs_local = new browserstack.Local();
+      exports.bs_local.start({'key': exports.config.commonCapabilities['browserstack.key'] }, function(error) {
+        if (error) return reject(error);
+        console.log('Connected. Now testing...');
+
+        resolve();
+      });
+    });
+  },
+
+  onPrepare: function() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
+
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
   },
-  onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+
+  // Code to stop browserstack local after end of test
+  afterLaunch: function() {
+    return new Promise(function(resolve, reject) {
+      exports.bs_local.stop(resolve);
+    });
   }
 };
+
+// Code to support common capabilities
+exports.config.multiCapabilities.forEach(function(caps){
+  for(var i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
+});
